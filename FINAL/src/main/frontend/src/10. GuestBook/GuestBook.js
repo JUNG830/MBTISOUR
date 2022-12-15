d660d3fb84c6eb2ec3a05199b7eb99e78ad196e4
9lovejhk <9lovejhk@naver.com>
2022-12-15 11:00:09 +0900

    221215 조혜경 1차

--- FINAL/src/main/frontend/src/10. GuestBook/GuestBook.js
+++ FINAL/src/main/frontend/src/10. GuestBook/GuestBook.js
@@ -1,117 +1,72 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import TeamAPI from "../0. API/TeamAPI";
 import Cookies from 'universal-cookie';
-import { useNavigate  } from "react-router-dom";
 import './GuestBook.css';
 import Moment from "react-moment";
-
-
+import basicFace from '../images/기본 프로필.png'
 
 const GuestBook = () => {
-    const [nickname, setNickname] = useState('');
-    const [content, setContent] = useState('');
-    const [chatInfo,setChatinfo] = useState([]);
-    const [isText, setIsText] = useState('');
-    const cookies = new Cookies();
-    const navigate = useNavigate();
-    const [face, setFace] = useState('');
+  const cookies = new Cookies();
+  const myInfo = cookies.get('rememberMyInfo');
+  const myId = myInfo.id
+  const myNickname = myInfo.nickname
+  const myFace = myInfo.face
 
-    const kakaoId_num = window.sessionStorage.getItem("kakaoId_num");
-    const localId = cookies.get('rememberId');
-    const name = cookies.get('nickname');
+  const [inputContent, setInputContent] = useState('');
+  const [guestbookList, setGuestbookList] = useState([]);
 
-  
-    useEffect(() => {
-        // if (localId === undefined) navigate("/login");
-    const memberData = async () => {
+  const onChangeText = e => {
+    let temp_text = e.target.value;
+    setInputContent(temp_text);
+  }
 
-        try {
-          const response = await TeamAPI.memberInfo(localId); // 원래는 전체 회원 조회용
-          setNickname(response.data.nickname)
-          setFace(response.data.face);
+  const onClickSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await TeamAPI.memberChat(inputContent,"내닉네임길잖아",myFace);
+      if(response.data === true) {
+        setInputContent('');
+        window.location.reload();
+      } else {
+        alert("통신 실패")
+      }
+    } catch(e) { console.log(e); }
+  }
+
+  useEffect(() => {
+    const chatData = async (e) => {
+      try {
+        const response = await TeamAPI.chatInfo(inputContent); // 원래는 전체 회원 조회용
+        if (response.status === 200) {
+          setGuestbookList(response.data);
+        }
+      } catch (e) { console.log(e); }
+    };
+  chatData();
+  }, [inputContent]);
     
-        } catch (e) {
-        }
-      };
-      memberData();
-    }, []);
+  return (
+    <div className="Container">
+      <div className="GuestBook-Container">
 
-    const onChangeText = e => {
-        let textShow = e.target.value;
+        <div className="GuestBook-header">
+          <input className="GuestBook-input" type="text" onChange={onChangeText} />
+          <button className ="GuestBook-submit" onClick={onClickSubmit} disabled={inputContent !== ""}>등록</button>
+        </div>
 
-        if(textShow === '') {
-            setIsText(false);
-        } else {
-            setContent(textShow);
-            setIsText(true);
-        }
-    }
-
-    const onClickBTN = async (data) => {
-        data.preventDefault();
-        try {
-            if( isText === true ) {
-                const res = await TeamAPI.memberChat(content,nickname,face);
-                if(res.data === true) {
-                    setContent('');
-                    setNickname(name);
-                    setFace(face);
-                    window.location.reload()
-                }else{
-                    alert("실패")
-                }
-               
-            } else {
-                alert('텍스트를 입력해주세요');
-                
-            
-        } }catch(e) {
-            alert("오류")
-        }
-    }
-
-    useEffect(() => {
-        
-        const chatData = async (e) => {
-
-          let sentence = content;
-         
-          try {
-            
-            const response = await TeamAPI.chatInfo(sentence); // 원래는 전체 회원 조회용
-           
-            if (response.status === 200) {
-            setChatinfo(response.data);
-            
-        }
-          } catch (e) {
-          }
-        };
-        chatData();
- 
-        }, [content]);
-    
-    
-    return (
-        <div className="chat-Container">
-            <div className="chat-box1">
-            <input className="gsend" disabled='' type="text" onChange={onChangeText}/>
-            <button className ="gbtn"  onClick={onClickBTN}>보내기</button>
-                <div className="minibox">
-                {chatInfo.map((chat) => (
-                    <div className="gchat" key={chat.id}>
-                            <div className="gchatNum">
-                            <img src={chat.face} alt="" style={{ marginTop: "1px",marginLeft:"auto",marginRight:"auto",width: "23px", height: "23px", border:"1px solid" ,borderRadius: "70%", overflow: "hidden", objectFit: "cover"}}/>
-                            {chat.nickname}
-                            </div>
-                            <div className="gcontent">{chat.content}
-                            <Moment format=" YYYY.MM.DD HH:mm" className="gchattime">{chat.chatTime}</Moment>
-                            </div>
-                    </div>
-                ))}
-                </div>
-            </div>
-            </div>
-    )
+        <div className="GuestBook-main">
+        {guestbookList.map((gb) => (
+          <div className="GuestBook-list" key={gb.id}>
+            <img src={gb.face ? gb.face : basicFace} alt="face" className="Guest-face" />
+            <div className="Guest-nickname">{gb.nickname}</div>
+            <div className="Guest-content">{gb.content}</div>
+            <Moment format=" YY.MM.DD" className="Guest-chatTime">{gb.chatTime}</Moment>
+          </div>
+        ))}
+        </div>
+      </div>
+    </div>
+  );
 }
+
 export default GuestBook;
