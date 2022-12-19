0568d744d5cfbcaa93e0c49b6410ae9d359e5ac8
dongkyunleee <coat000@naver.com>
2022-12-19 16:32:18 +0900

    2022/12/19 이동균

--- FINAL/src/main/frontend/src/1. Main/Main.js
+++ FINAL/src/main/frontend/src/1. Main/Main.js
@@ -16,28 +16,28 @@ function Main () {
             <div className="Main_title">
 <ScrollContainer>
     <ScrollPage>
-        <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -250))}>
+        <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -300))}>
             <span style={{ fontSize: "30px" }}>MBTISOUR</span>
         </Animator>
     </ScrollPage>
     
     <ScrollPage>
-        <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -250))}>
+        <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -300))}>
             <span style={{ fontSize: "30px" }}> <strong><p>인간들은 성격이 존재합니다.</p></strong></span>
         </Animator>
     </ScrollPage>
     <ScrollPage>
-        <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -250))}>
+        <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -300))}>
             <span className="Page3" style={{ fontSize: "30px" }}> <strong><p>안맞는 사람들이 있을거고</p></strong></span>
         </Animator>
     </ScrollPage>
     <ScrollPage>
-        <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -250))}>
+        <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -300))}>
             <span className="Page3" style={{ fontSize: "30px" }}> <strong><p>또 잘맞는 사람이 있습니다.</p></strong></span>
         </Animator>
     </ScrollPage>
     <ScrollPage>
-        <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -250))}>
+        <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -300))}>
             <span className="Page3" style={{ fontSize: "30px" }}> <strong><p>내 성격이 궁금하신가요?</p></strong></span>
         </Animator>
     </ScrollPage>
