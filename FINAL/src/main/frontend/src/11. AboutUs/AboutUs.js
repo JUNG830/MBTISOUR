564d34c0eb491095ecee52266d2033165bc5f58e
9lovejhk <9lovejhk@naver.com>
2022-12-20 10:10:08 +0900

    221220 조혜경 2차

--- FINAL/src/main/frontend/src/11. AboutUs/AboutUs.js
+++ FINAL/src/main/frontend/src/11. AboutUs/AboutUs.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import TeamAPI from '../0. API/TeamAPI';
 import Cookies from 'universal-cookie';
 import {MatchingPostModal} from '../99. Modal/MatchingPostModal';
+import CustomModal from '../99. Modal/CustomModal'
 import 조혜경 from '../images/조혜경.png'
 import 이동균 from '../images/이동균.png'
 import 이민형 from '../images/이민형.png'
@@ -17,6 +18,17 @@ function AboutUs() {
   const MyInfo = cookies.get('rememberMyInfo');
   const myId = MyInfo.id;
 
+  /* ===== CustomModal 에 필요 ===== */
+  const [state, setState] = useState({
+    open: false, success: false, error: false,
+    successMsg: "", errorMsg: ""
+  });
+
+  const onChangeState = () => {
+    setState({...state, open: false, success: false, error: false});
+  }
+  /* ============================== */
+
   /* 쪽지 기능 구현 */
   const [receiverId, setReceiverId] = useState("");
   const [receiverNickname, setReceiverNickname] = useState("");
@@ -29,7 +41,6 @@ function AboutUs() {
   const closeModal = () => { setModalOn(false); };
 
   const onClickPostIcon = (receiverId, receiverNickname) => {
-    alert("쪽지보내실?");
     console.log("\n>> 쪽지 아이콘 눌렀어요.");
 
     setReceiverId(receiverId);
@@ -52,7 +63,7 @@ function AboutUs() {
       if(response.status == 200) {
         console.log("통신 성공(200)");
         console.log("\n>> 쪽지 보내기 성공!!");
-        alert("쪽지 보내기 성공!!");
+        setState({...state, open: true, success: true, successMsg: "쪽지가 발송되었습니다."});
         closeModal();
       }
     } catch (e) {
@@ -62,51 +73,50 @@ function AboutUs() {
 
   return (
     <div className='Container'>
-      <div className='Middle-Container'>
-        <div className='AboutUs-Container'>
-          <div className='AboutUs-header'>
-            <h1>About Us</h1>
-          </div>
-          <div className='AboutUs-main'>
-            <div className='OurInfo'>
-              <img src={우혜정} alt="face"/>
-              <h4>우혜정</h4>
-              <h6>Backend / Frontend</h6>
-              <CallIcon />              
-              <span class="material-symbols-outlined" onClick={()=>onClickPostIcon("kkongkkong", "손이시려워")}>mail</span>
-            </div>
-            <div className='OurInfo'>
-              <img src={조혜경} alt="face"/>
-              <h4>조혜경</h4>
-              <h6>Backend / Frontend</h6>
-              <CallIcon />
-              <span class="material-symbols-outlined" onClick={()=>onClickPostIcon("9lovejhk", "개발자조혜경")}>mail</span>
-            </div>
-            <div className='OurInfo'>
-              <img src={전규한} alt="face"/>
-              <h4>전규한</h4>
-              <h6>Backend / Frontend</h6>
-              <CallIcon />
-              <span class="material-symbols-outlined" onClick={()=>onClickPostIcon("kongdole", "콩돌형")}>mail</span>
-            </div>
-            <div className='OurInfo'>
-              <img src={이민형} alt="face"/>
-              <h4>이민형</h4>
-              <h6>Backend / Frontend</h6>
-              <CallIcon />
-              <span class="material-symbols-outlined" onClick={()=>onClickPostIcon("", "")}>mail</span>
-            </div>
-            <div className='OurInfo'>
-              <img src={이동균} alt="face"/>
-              <h4>이동균</h4>
-              <h6>Backend / Frontend</h6>
-              <CallIcon />
-              <span class="material-symbols-outlined" onClick={()=>onClickPostIcon("", "")}>mail</span>
-            </div>
-          </div>
-          <MatchingPostModal open={modalOn} close={closeModal} receiver={receiverNickname} getInputContent={getInputContent} onSendPost={onSendPost}/>
+      <div className='AboutUs-Container'>
+        <div className='AboutUs-header'>
+          <h1>About Us</h1>
         </div>
+        <div className='AboutUs-main'>
+          <div className='OurInfo'>
+            <img src={우혜정} alt="face"/>
+            <h4>우혜정</h4>
+            <h6>Backend / Frontend</h6>
+            <CallIcon />              
+            <span class="material-symbols-outlined" onClick={()=>onClickPostIcon("kkongkkong", "손이시려워")}>mail</span>
+          </div>
+          <div className='OurInfo'>
+            <img src={조혜경} alt="face"/>
+            <h4>조혜경</h4>
+            <h6>Backend / Frontend</h6>
+            <CallIcon />
+            <span class="material-symbols-outlined" onClick={()=>onClickPostIcon("9lovejhk", "개발자조혜경")}>mail</span>
+          </div>
+          <div className='OurInfo'>
+            <img src={전규한} alt="face"/>
+            <h4>전규한</h4>
+            <h6>Backend / Frontend</h6>
+            <CallIcon />
+            <span class="material-symbols-outlined" onClick={()=>onClickPostIcon("kongdole", "콩돌형")}>mail</span>
+          </div>
+          <div className='OurInfo'>
+            <img src={이민형} alt="face"/>
+            <h4>이민형</h4>
+            <h6>Backend / Frontend</h6>
+            <CallIcon />
+            <span class="material-symbols-outlined" onClick={()=>onClickPostIcon("", "")}>mail</span>
+          </div>
+          <div className='OurInfo'>
+            <img src={이동균} alt="face"/>
+            <h4>이동균</h4>
+            <h6>Backend / Frontend</h6>
+            <CallIcon />
+            <span class="material-symbols-outlined" onClick={()=>onClickPostIcon("", "")}>mail</span>
+          </div>
+        </div>
+        <MatchingPostModal open={modalOn} close={closeModal} receiver={receiverNickname} getInputContent={getInputContent} onSendPost={onSendPost}/>
       </div>
+      <CustomModal state={state} changeState={onChangeState}/>
     </div>
   )
 }
