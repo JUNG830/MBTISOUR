6df357c531cf75f15723b0869b9c6865feadd7c3
dongkyunleee <coat000@naver.com>
2022-12-23 18:29:00 +0900

    홈 배경 추가

--- FINAL/src/main/frontend/src/4. Home/Home.js
+++ FINAL/src/main/frontend/src/4. Home/Home.js
@@ -2,19 +2,28 @@ import { useState, useEffect } from "react";
 import TeamAPI from '../0. API/TeamAPI';
 import Cookies from 'universal-cookie';
 import { db } from "../firebase";
+import {
+  collection,
+  query,
+  where,
+  onSnapshot,
+  addDoc,
+  Timestamp,
+  orderBy,
+  setDoc,
+  doc,
+  getDoc,
+  updateDoc
+} from "firebase/firestore";
 import moment from 'moment';
 import './Home.css';
-import Loading from '../other/Loading';
 import { useNavigate } from "react-router-dom";
-
-
+import Snow from '../Snow/Snow.css'
 const Home = () => {
 
-  const [loading, setLoading] = useState(false);
   const cookies = new Cookies();
-  
-  const localId = cookies.get('rememberId');
 
+  const localId = cookies.get('rememberId');
   const localId_num = window.sessionStorage.getItem("id_num");
   const kakaoId_num = window.sessionStorage.getItem("kakaoId_num");
   const kakaoNickname = window.sessionStorage.getItem("kakaoNickname");
@@ -22,28 +31,26 @@ const Home = () => {
   const kakaoEmail = window.sessionStorage.getItem("kakaoEmail");
   const [friend, setFriend] = useState("");
   const [registrationDate, setRegistrationDate] = useState('');
-  const [nickname, setNickname] = useState('');
+  const [nickName, setNickName] = useState('');
   const navigate = useNavigate();
-
-  console.log(cookies.get('rememberMyInfo'));
 
   useEffect(() => {
     if (localId === undefined) navigate("/login");
     // ▲ 로그인 안 되어 있으면 로그인 페이지로 
 
     const memberData = async () => {
-      setLoading(true);
       try {
         const response = await TeamAPI.memberInfo(localId); // 원래는 전체 회원 조회용
-        cookies.set('rememberMyInfo', response.data);
-        setNickname(response.data.nickname);
+        setNickName(response.data.nickname)
         setRegistrationDate(response.data.registrationDate);
-        console.log(cookies.get('rememberMyInfo'));
+        cookies.set('rememberMyInfo', response.data, {
+          path: '/',
+          expires: 0
+        })
 
       } catch (e) {
         console.log(e);
       }
-      setLoading(false);
     };
     memberData();
   }, []);
@@ -51,22 +58,15 @@ const Home = () => {
   const date = moment().format("YYYY.MM.DD HH:mm:ss");
   const Dday = moment(date).diff(registrationDate, 'day');
 
-  if (loading) {
-    return (
-      <Loading />
-    );
-  }
-
   return (
-    <div className="Container">
+    <div>
+    <div className="Container snow">
+      
       <div className="Home-Container">
-        
         <div className="WelcomeMessage">
-          <h2><span style={{ color: "navy", fontWeight: "bold" }}>{nickname}</span> 님과 MBTISOUR는 오늘, <span style={{ color: 'red', fontWeight: 'bold' }}>{Dday + 1}일</span></h2>
-          {/* <h2><span style={{ color: "navy", fontWeight: "bold" }}>닉네임</span> 님과 MBTISOUR는 오늘, <span style={{ color: 'red', fontWeight: 'bold' }}>몇 일</span></h2> */}
+          <h2><span style={{ color: "navy", fontWeight: "bold" }}>{nickName}</span> 님과 MBTISOUR는 오늘, <span style={{ color: 'red', fontWeight: 'bold' }}>{Dday + 1}일</span></h2>
           {/* <h3>새로운 쪽지가 있습니다!</h3> */}
         </div>
-
         <div className="Dino">
           <iframe className="DinoAddr" src="https://chromedino.com/" type="text/css" frameborder="0" scrolling="no" width="100%" height="100%" loading="lazy"></iframe>
         </div>
@@ -78,6 +78,7 @@ const Home = () => {
         </div>
 
       </div>
+    </div>
     </div>
   );
 }
